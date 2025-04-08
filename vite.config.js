5fed84ac8a70fae065e5d3138975dc910852ca62
Akos Marton <akos.marton@gmail.com>
2025-04-08 09:28:48 +0300

    Remove unused CSS

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
+import pluginPurgeCss from "@myelophone/vite-plugin-purgecss";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [svelte(), pluginPurgeCss()],
   build: {
     chunkSizeWarningLimit: 1200,
     rollupOptions: {
